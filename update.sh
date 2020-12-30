1e8d3346cbfb9f2a69ba47d7bc790918d836af9a
Robbie Blaine <robbie@zenaptix.com>
2020-12-30 19:44:51 +0200

    Update Action Script
    * Add date and time of update to top of blocklists

--- update.sh
+++ update.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-wget https://gui.xmr.pm/files/block.txt -O block.txt
-wget https://gui.xmr.pm/files/block_tor.txt -O block_tor.txt
+TIME=$(date -u +"%Y-%m-%dT%H:%M:%S+00:00")
+
+printf "# Updated at ${TIME}\n" > block.txt
+wget https://gui.xmr.pm/files/block.txt -O - >> block.txt
+
+printf "# Updated at ${TIME}\n" > block_tor.txt
+wget https://gui.xmr.pm/files/block_tor.txt -O - >> block_tor.txt
 
 git add .
-date=$(date -u -Iseconds)
-git commit --allow-empty -m ${date}
+git commit --allow-empty -m ${TIME}
 git push
